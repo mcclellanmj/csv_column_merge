523e01313cc5cde25f8c964d0ec396b0f19d9262
Matt McClellan <mcclellan.mj@gmail.com>
2014-06-26 11:54:04 -0500

    Added flag to set the delimiter used to seperate merged cells

--- merge_csv_columns.py
+++ merge_csv_columns.py
@@ -5,8 +5,10 @@ import argparse
 parser = argparse.ArgumentParser(description='CSV column merge')
 parser.add_argument('merge_columns', nargs='+', help='Columns to be merged', type=int)
 parser.add_argument('--ignore-empty', action='store_true', help='Will not join the particular cell if it is completely empty')
+parser.add_argument('-d', '--delimiter', help='The delimiter that will be used between merged cells', default=' ')
 arguments = parser.parse_args()
 merge_columns = [i - 1 for i in arguments.merge_columns]
+delimiter = arguments.delimiter
 
 def include(cell):
 	if arguments.ignore_empty:
@@ -16,7 +18,7 @@ def include(cell):
 
 def merged_generator(source, *to_merge):
 	for row in source:
-		merged_columns = " ".join([row[i] for i in to_merge if include(row[i])])
+		merged_columns = delimiter.join([row[i] for i in to_merge if include(row[i])])
 		row[to_merge[0]] = merged_columns
 		yield [column for (i, column) in enumerate(row) if not (i in to_merge[1:])]
 		
