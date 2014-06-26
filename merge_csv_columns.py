da638f17c7c91635ffe701ac5e1362737e2399ac
Matt McClellan <mcclellan.mj@gmail.com>
2014-06-26 09:24:10 -0500

    - Cleaned up code - Fixed typo

--- merge_csv_columns.py
+++ merge_csv_columns.py
@@ -4,7 +4,7 @@ import argparse
 
 parser = argparse.ArgumentParser(description='CSV column merge')
 parser.add_argument('merge_columns', nargs='+', help='Columns to be merged', type=int)
-parser.add_argument('--ignore-empty', action='store_true', help='Will not join the particular cell if it is completly empty')
+parser.add_argument('--ignore-empty', action='store_true', help='Will not join the particular cell if it is completely empty')
 arguments = parser.parse_args()
 merge_columns = [i - 1 for i in arguments.merge_columns]
 
@@ -18,8 +18,7 @@ def merged_generator(source, *to_merge):
 	for row in source:
 		merged_columns = " ".join([row[i] for i in to_merge if include(row[i])])
 		row[to_merge[0]] = merged_columns
-		to_yield = [column for (i, column) in enumerate(row) if not (i in to_merge[1:])]
-		yield to_yield
+		yield [column for (i, column) in enumerate(row) if not (i in to_merge[1:])]
 		
 
 reader = csv.reader(iter(sys.stdin.readline, ''))
